d802bd7b1982c7fef1588dd6905fcf99a2ceef45
Byron <cm9930914@gmail.com>
2021-06-15 05:20:53 -0700

    0615

---
+++ HW4_2_line_detction/main.cpp
@@ -0,0 +1,32 @@
+#include "mbed.h"
+#include "bbcar.h"
+#include "bbcar_rpc.h"
+
+Ticker servo_ticker;
+PwmOut pin5(D5), pin6(D6);
+BufferedSerial pc(USBTX, USBRX); //tx,rx
+BufferedSerial uart(D1, D0);     //tx,rx
+BBCar car(pin5, pin6, servo_ticker);
+
+int main()
+{
+    uart.set_baud(9600);
+    char buf[256], outbuf[256];
+    FILE *devin = fdopen(&uart, "r");
+    FILE *devout = fdopen(&uart, "w");
+    while (1)
+    {
+        memset(buf, 0, 256);
+        for (int i = 0;; i++)
+        {
+            char recv = fgetc(devin);
+            if (recv == '\n')
+            {
+                printf("\r\n");
+                break;
+            }
+            buf[i] = fputc(recv, devout);
+        }
+        RPC::call(buf, outbuf);
+    }
+}
